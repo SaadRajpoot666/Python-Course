61f27f2aa1abf54ee434a45431cb7e30af638db9
Saad Bin Khalid <khalidmehmoodeditz@gmail.com>
2025-04-29 12:34:31 +0500

    Initial commit

---
+++ first.py
@@ -0,0 +1,20 @@
+# # import pyjokes
+# # print("Hello World!")
+
+# # # making joke 
+# # joke= pyjokes.get_joke()
+# # print(joke)
+# print('''twinkle twinkle little start
+# how i wonder that you are 
+# up above the wonder sky
+# like a diamond in the sky''')
+
+# import pyttsx3
+# engine = pyttsx3.init()
+
+# a = input("Enter the message: ")
+
+
+# engine.say(a)
+# engine.runAndWait()
+print(1 + 1)
