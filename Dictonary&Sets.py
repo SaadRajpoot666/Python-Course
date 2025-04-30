ae13c7c2a9ee6497908f9658c286cc90edc1d1b6
Saad Bin Khalid <khalidmehmoodeditz@gmail.com>
2025-04-30 11:39:51 +0500

    Chapter 5 Completed

---
+++ Dictonary&Sets.py
@@ -0,0 +1,15 @@
+print("Dictonary and Sets")
+
+#dictionaries in python and objects in js
+
+marks = {
+    "phy":70,
+    "maths" :90
+} 
+marks["maths"] = 85
+print(marks["maths"])
+
+s = set()
+s.add(23)
+s.clear()
+print(s)
---
+++ chapter5PracSet.py
@@ -0,0 +1,21 @@
+print("chapter 5 practise set")
+
+# #-> pracise number 1
+# word = {
+#     "nam" :"name",
+#     "kela" :"Banana",
+#     "aam" :"Mango"
+# }
+# wordInput = input("Enter the Word whose meaning you want in english: ")
+
+# print(word[wordInput])
+#-> Practise number 2
+
+n1 = input("Enter the number 1 :")
+n2 = input("Enter the number 2 :")
+n3 = input("Enter the number 3 :")
+n4 = input("Enter the number 4 :")
+
+s= set()
+s.update(n1,n2,n3,n4)
+print(s)
