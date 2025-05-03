56e91670ae0568e6681ba01f5911a0ac8d30d5f8
Saad Bin Khalid <khalidmehmoodeditz@gmail.com>
2025-05-03 11:00:59 +0500

    Chapter 7 Completed

---
+++ Loops.py
@@ -0,0 +1,33 @@
+print("Chapter number 7 Loops")
+
+# demo
+# for i in range(1,6):
+#     print(i)
+    
+# print("This is the While loop")
+# # while loop
+# i = 1
+# # while(i<=50):
+# #     print(i)
+# #     i+=1 
+# j=0 
+# l = ["Saad","Raza","Hashir"]
+# # while(j<len(l)):
+# #     print(l[j])
+# #     j+=1 
+
+
+# for i in l:
+#     print(i)
+# else:
+#     print("done")
+
+for i in range(100):
+    if(i == 4):
+        break
+    print(i)
+    
+for i in range(100):
+    if(i == 4):
+        continue
+    print(i)
